d5509668e1f2000d69c1a2927c7b25cee7d3db31
Samer Ghaly <samereaghaly@gmail.com>
2023-02-16 19:28:37 +0200

    Create and build the MemePreview component

---
+++ src/Components/MemePreview.jsx
@@ -0,0 +1,11 @@
+function MemePreview(){
+    return(
+        <section className="memePreview">
+            <img src="./src/assets/meme-img.png" alt="meme image" className="memeImg" />
+            <p className="topText"></p>
+            <p className="bottomText"></p>
+        </section>
+    )
+}
+
+export default MemePreview
