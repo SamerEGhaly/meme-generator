437cef8fe4f6db687a4cd255c09ddd7addafaf75
Samer Ghaly <samereaghaly@gmail.com>
2023-02-16 19:08:28 +0200

    Create and build the NavBar component

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,13 @@
+function NavBar(){
+    return(
+        <nav className="navBar">
+            <div className="logo">
+                <img src="./src/assets/troll-face.png" alt="troll face" className="trollFace" />
+                <h3 className="title">Meme Generator</h3>
+            </div>
+            <p>React Course - Project 3</p>
+        </nav>
+    )
+}
+
+export default NavBar
