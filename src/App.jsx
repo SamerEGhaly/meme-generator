e0fcf01cafb8cfa4094602a207e1ff096454fbe7
Samer Ghaly <samereaghaly@gmail.com>
2023-03-01 12:41:12 +0200

    remove import statements for deleted files

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import NavBar from "./Components/NavBar"
-import MemeEditor from "./Components/MemeEditor"
-import MemePreview from "./Components/MemePreview"
 import Meme from "./Components/Meme"
 
 function App() {
